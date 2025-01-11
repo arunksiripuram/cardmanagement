43a48b5a05a0b352ecaad4abe3848973dc9b5d48
arunk.siripuram <arunk.siripuram@gmail.com>
2025-01-11 20:44:36 +0530

    push

--- CardManagementAPI/Program.cs
+++ CardManagementAPI/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddCors(options =>
   options.AddPolicy("AllowAll", builder =>
   {
     builder.AllowAnyOrigin()
+            .AllowCredentials()
            .AllowAnyMethod()
            .AllowAnyHeader();
   });
