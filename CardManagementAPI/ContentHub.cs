bd90c165cd6746c31f4cfba62fea68e4282d1883
arunk.siripuram <arunk.siripuram@gmail.com>
2025-01-12 16:29:30 +0530

    arun: update gg

--- CardManagementAPI/ContentHub.cs
+++ CardManagementAPI/ContentHub.cs
@@ -3,9 +3,14 @@ using Microsoft.AspNetCore.SignalR;
 
 public class ContentHub : Hub
 {
-    // This method will send updates to all connected clients
-    public async Task SendContentUpdate(ContentModel content)
-    {
-        await Clients.All.SendAsync("ReceiveContentUpdate", content);
-    }
+  // This method will send updates to all connected clients
+  public async Task SendContentUpdate(ContentModel content)
+  {
+    await Clients.All.SendAsync("ReceiveContentUpdate", content);
+  }
+  // This method sends label updates to all connected clients
+  public async Task SendLabelUpdate(Dictionary<string, string> labels)
+  {
+    await Clients.All.SendAsync("ReceiveLabelUpdate", labels);
+  }
 }
